dc23c45d319b62c91aa56c33c757f54467f4d9d7
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-10-12 18:03:34 -0300

    github actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,14 @@ terraform {
       version = "~> 2.0"
     }
   }
+
+  backend "remote" {
+    organization = "felippedeiro"
+
+    workspaces {
+      name = "rotten-potatoes"
+    }
+  }
 }
 
 #variavel para o token nao ficar exposto num arquivo compartilhavel
